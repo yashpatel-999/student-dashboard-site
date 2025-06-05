df3af97714b85ac9b8cd9607574011a0f4c19c5b
yashpatel-999 <thotayash47@gmail.com>
2025-06-05 10:40:11 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'student-dashboard';
+  name = '';
+  selectedGrade = '';
+  students: { name: string; grade: string }[] = [];
+  onSubmit() {
+      this.students.push({ name: this.name, grade: this.selectedGrade });
+      this.name = '';
+      this.selectedGrade = '';
+  }
 }
